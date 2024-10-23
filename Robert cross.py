eba0fd1eb558be36c1da5fc4810a1ba1fa26c455
Vee6066 <yogaradtyaav@gmail.com>
2024-10-23 14:55:09 +0800

    Robert cross.py

--- Robert cross.py
+++ Robert cross.py
@@ -9,7 +9,7 @@ import cv2
 import numpy as np
 
 # Membaca gambar dari file
-img = cv2.imread('C:/Users/Lenovo/Pictures/Camera Roll/WIN_20240625_08_15_09_Pro.jpg', 100)
+img = cv2.imread('C:\\Users\\lenov\\Pictures\\Camera Roll\\Saved Pictures\\pryg.jpg'', 100)
 
 # Kernel untuk operator Roberts Cross
 roberts_cross_x = np.array([[1, 0], [0, -1]], dtype=np.float32)  # Kernel untuk arah X
